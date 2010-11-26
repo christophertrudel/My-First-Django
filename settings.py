ae04c1a270631308ac7eadadbf1a860f509f125d
Christopher Trudel <christophertrudel@christophertrudel.local>
2010-11-25 21:26:37 -0330

    more messing around

--- settings.py
+++ settings.py
@@ -11,12 +11,12 @@ MANAGERS = ADMINS
 
 DATABASES = {
 	'default': {
-		'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+		'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
 		'NAME': 'myfirstdjango',                      # Or path to database file if using sqlite3.
 		'USER': 'root',                      # Not used with sqlite3.
 		'PASSWORD': '',                  # Not used with sqlite3.
 		'HOST': '127.0.0.1',                      # Set to empty string for localhost. Not used with sqlite3.
-		'PORT': '3306',                      # Set to empty string for default. Not used with sqlite3.
+		'PORT': '3307',                      # Set to empty string for default. Not used with sqlite3.
 	}
 }
 
--- urls.py
+++ urls.py
@@ -7,10 +7,12 @@ admin.autodiscover()
 urlpatterns = patterns('',
 	# Example:
 	# (r'^my_first_django/', include('my_first_django.foo.urls')),
-
-	# Uncomment the admin/doc line below to enable admin documentation:
-	(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-	# Uncomment the next line to enable the admin:
+	(r'^polls/$', 'polls.views.index'),
+	(r'^polls/(?P<poll_id>\d+)/$', 'polls.views.detail'),
+	(r'^polls/(?P<poll_id>\d+)/results/$', 'polls.views.results'),
+	(r'^polls/(?P<poll_id>\d+)/vote/$', 'polls.views.vote'),
+	
+	# Uncomment the next two lines to enable the admin and the admin/doc respectivly:
 	(r'^admin/', include(admin.site.urls)),
+	(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 )
